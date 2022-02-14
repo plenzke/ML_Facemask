f01429a5bd3efd1fd27a4188f1e83bfd6c47bec1
plenzke <3njoyth3sil3nc399@gmail.com>
2022-02-14 17:04:11 +0900

    data preparation

---
+++ model.py
@@ -0,0 +1,40 @@
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+DIRECTORY = r"C:\Users\Admin\Desktop\ML_Facemask\dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
+data = []
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+    	img_path = os.path.join(path, img)
+    	image = load_img(img_path, target_size=(224, 224))
+    	image = img_to_array(image)
+    	image = preprocess_input(image)
+
+    	data.append(image)
+    	labels.append(category)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
